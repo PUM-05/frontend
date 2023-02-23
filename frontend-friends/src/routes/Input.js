ce78592120091c65a2388bac13ceb5d76b745471
Mattias <malin153@student.liu.se>
2023-02-23 12:05:51 +0100

    categorybuttons

--- frontend-friends/src/routes/Input.js
+++ frontend-friends/src/routes/Input.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import CategoryButton from "../components/categoryButton/CategoryButton";
 
 export default function Input() {
   return (
     <>
+      <CategoryButton id="hej" name="knappar" color="red"> Hej </CategoryButton>
       <h1>Inmatning</h1>
       <Link to="/dashboard">Gå till överblickssida</Link>
+      <CategoryButton id="test" name="knappar" color="darkblue">Kategori 1</CategoryButton>
+      <CategoryButton id="test2" name="knappar" color="darkblue">Kategori 2</CategoryButton>
     </>
   );
 }
