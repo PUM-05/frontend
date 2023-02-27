c71bbd86d61e80ea6938bca0c39870163d851cec
Johannes Pettersson <johpe243@student.liu.se>
2023-02-27 11:38:36 +0100

    Commit for general design changes

--- frontend-friends/src/components/categoryButton/CategoryButton.js
+++ frontend-friends/src/components/categoryButton/CategoryButton.js
@@ -2,9 +2,9 @@ import "./button.scss";
 
 export default function CategoryButton (props){
     return (
-        <>
-        <input id={props.id} name={props.name} type="radio" className="red active"/>
-        <label for={props.id} name={props.name} className={`${props.color}`}>{props.children}</label>
-        </>
+        <div className="category-button-wrapper">
+            <input id={props.id} name={props.name} type="radio" className="red active"/>
+            <label for={props.id} name={props.name} className={`${props.color}`}>{props.children}</label>
+        </div>
     )
 }
--- frontend-friends/src/components/textfield/TextField.js
+++ frontend-friends/src/components/textfield/TextField.js
@@ -1,6 +1,12 @@
 import "./textfield.scss"
-export default function TextField() {
+export default function TextField(props) {
+    const isRequired = props.isRequired;
+    if (isRequired) {
+        return (
+            <input type="text" placeholder={props.placeholder} className="text-field" required />          
+        );
+    }
     return (
-        <input type="text" placeholder="Ärendenr" className="text-field"/>          
+        <input type="text" placeholder={props.placeholder} className="text-field" />    
     );
 }
