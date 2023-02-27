2b9932c331aa823fc092ec08529634b52519671f
Pontus Ferm <ponfe408@student.liu.se>
2023-02-27 19:10:33 +0100

    Started working on functionality supporting subcategories

--- frontend-friends/src/components/categoryButton/CategoryButton.js
+++ frontend-friends/src/components/categoryButton/CategoryButton.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./button.scss";
 
 export default function CategoryButton (props){
+
+    const [clicked, setClicked] = useState(false);
+
     return (
         <div className="category-button-wrapper">
-            <input id={props.id} name={props.name} type="radio" className="red active"/>
+            <input id={props.id} name={props.name} type="radio" value={props.value} className="red active" onClick={(clicked) => props.onClick?.(clicked)}/>
             <label for={props.id} name={props.name} className={`${props.color}`}>{props.children}</label>
         </div>
     )
