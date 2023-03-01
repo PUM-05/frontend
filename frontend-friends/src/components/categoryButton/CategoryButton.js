812e0cc05ac2be62c063dae07018ebbb83d65cf3
unknown <ponfe408@student.liu.se>
2023-03-01 16:56:19 +0100

    Changed method for the functionality

--- frontend-friends/src/components/categoryButton/CategoryButton.js
+++ frontend-friends/src/components/categoryButton/CategoryButton.js
@@ -3,12 +3,10 @@ import "./button.scss";
 
 export default function CategoryButton (props){
 
-    const [clicked, setClicked] = useState(false);
-
     return (
         <div className="category-button-wrapper">
-            <input id={props.id} name={props.name} type="radio" value={props.value} className="red active" onClick={(clicked) => props.onClick?.(clicked)}/>
-            <label for={props.id} name={props.name} className={`${props.color}`}>{props.children}</label>
+            <input id={props.id} name={props.name} value={props.value} type="radio" className="red active" onClick={(clicked) => props.onClick?.(clicked)}/>
+            <label for={props.id} name={props.name} value={props.value} className={`${props.color}`}>{props.children}</label>
         </div>
     )
 }
--- frontend-friends/src/components/categoryButtonGroup/CategoryButtonGroup.js
+++ frontend-friends/src/components/categoryButtonGroup/CategoryButtonGroup.js
@@ -10,19 +10,42 @@ export default function CategoryButtonGroup (props){
 
     
     const handleClick = event => {
-        clickedButton = event.currentTarget.id;
+        clickedButton = event.currentTarget;
+        console.log(event.currentTarget)
         setBool1(!bool1);
     } 
 
     const subcategoryClick = event => {
-        let buttonText = event.currentTarget.value;
-        clickedButton = buttonText;
+        //let buttonText = event.currentTarget.value;
+        //clickedButton.value = buttonText;
+        console.log(event.currentTarget.value);
+        console.log(clickedButton);
         setBool1(!bool1);
     }
 
     return (
         <div className="top-button-group-wrapper">
-            {bool1 ? 
+                <div className="button-group-wrapper">
+                    <CategoryButton id="Knapp1" value="Justera lön" name={props.name} color="darkblue">Justera lön</CategoryButton>
+                    <CategoryButton id="Knapp2" value="Registrera frånvaro" name={props.name} color="orange">Registrera frånvaro</CategoryButton>
+                    <CategoryButton id="Knapp3" name={props.name} color="red">Registrera ny personal</CategoryButton>
+                    <CategoryButton id="Knapp4" name={props.name} color="green" onClick={handleClick}>Stämpelklocka</CategoryButton>
+                    {!bool1 ? <div className="break"></div> : null}
+                    {!bool1 ? <CategoryButton id="Knapp10" value="Problem med stämpelklocka" name={props.name} color="yellow" onClick={subcategoryClick}>Problem</CategoryButton> : null}
+                    {!bool1 ? <CategoryButton id="Knapp11" value="Installation av stämpelklocka" name={props.name} color="darkblue" onClick={() => subcategoryClick("Knapp4")}>Installation</CategoryButton> : null}
+                    {!bool1 ? <CategoryButton id="Knapp12" value="Synkronisering av stämpelklocka"name={props.name} color="darkblue" onClick={() => subcategoryClick("Knapp4")}>Synkronisering</CategoryButton> : null}
+                    <CategoryButton id="Knapp5" name={props.name} color="darkblue">Ändra inställningar</CategoryButton>
+                    <CategoryButton id="Knapp6" name={props.name} color="darkblue">Skapa nytt konto</CategoryButton>
+                    <CategoryButton id="Knapp7" name={props.name} color="darkblue">Ta bort konto</CategoryButton>
+                    <CategoryButton id="Knapp8" name={props.name} color="darkblue">Exportera data</CategoryButton>
+                    <CategoryButton id="Knapp9" name={props.name} color="darkblue">Annat</CategoryButton>
+                </div>       
+        </div>
+    )
+}
+
+/*
+    {bool1 ? 
                 <div className="button-group-wrapper">
                     <CategoryButton id="Knapp1" value="Justera lön" name={props.name} color="darkblue">Justera lön</CategoryButton>
                     <CategoryButton id="Knapp2" value="Registrera frånvaro" name={props.name} color="orange">Registrera frånvaro</CategoryButton>
@@ -38,33 +61,10 @@ export default function CategoryButtonGroup (props){
             }
             {!bool1 ? <div className="subcategory-popup-1">
                 <button id="back" onClick={() => setBool1(!bool1)}>Tillbaka</button>
-                <CategoryButton id="Knapp10" value="Problem med stämpelklocka" name={props.name} color="yellow" onClick={subcategoryClick}>Problem</CategoryButton>
-                <CategoryButton id="Knapp11" value="Installation av stämpelklocka" name={props.name} color="darkblue" onClick={subcategoryClick}>Installation</CategoryButton>
-                <CategoryButton id="Knapp12" value="Synkronisering av stämpelklocka"name={props.name} color="darkblue" onClick={subcategoryClick}>Synkronisering</CategoryButton>
+                <CategoryButton id="Knapp10" value="Problem med stämpelklocka" name={props.name} color="yellow" onClick={() => subcategoryClick("Knapp4")}>Problem</CategoryButton>
+                <CategoryButton id="Knapp11" value="Installation av stämpelklocka" name={props.name} color="darkblue" onClick={() => subcategoryClick("Knapp4")}>Installation</CategoryButton>
+                <CategoryButton id="Knapp12" value="Synkronisering av stämpelklocka"name={props.name} color="darkblue" onClick={() => subcategoryClick("Knapp4")}>Synkronisering</CategoryButton>
                 
                 </div>
-            : null}
-        </div>
-    )
-}
-
-
-/*
-    return (
-        <div className="button-group-wrapper">
-            {bool1 ? <CategoryButton id="Knapp1" value="1" name={props.name} color="darkblue">Justera lön</CategoryButton> : null}
-            {bool1 ? <CategoryButton id="Knapp2" name={props.name} color="orange">Registrera frånvaro</CategoryButton> : null}
-            {bool1 ? <CategoryButton id="Knapp3" name={props.name} color="red">Registrera ny personal</CategoryButton> : null}
-            <CategoryButton id="Knapp12" name={props.name} color="green" onClick={handleClick}>Stämpelklocka</CategoryButton>
-            {!bool1 ? <br/> : null}
-            {!bool1 ? <CategoryButton id="Knapp4" name={props.name} color="yellow">Problem med stämpelklocka</CategoryButton> : null}
-            {!bool1 ? <CategoryButton id="Knapp5" name={props.name} color="darkblue">Installation av stämpelklocka</CategoryButton> : null}
-            {!bool1 ? <CategoryButton id="Knapp6" name={props.name} color="darkblue">Synkronisering av stämpelklocka</CategoryButton> : null}
-            {bool1 ? <CategoryButton id="Knapp7" name={props.name} color="darkblue">Ändra inställningar</CategoryButton> : null}
-            {bool1 ? <CategoryButton id="Knapp8" name={props.name} color="darkblue">Skapa nytt konto</CategoryButton> : null}
-            {bool1 ? <CategoryButton id="Knapp9" name={props.name} color="darkblue">Ta bort konto</CategoryButton> : null}
-            {bool1 ? <CategoryButton id="Knapp10" name={props.name} color="darkblue">Exportera data</CategoryButton> : null}
-            {bool1 ? <CategoryButton id="Knapp11" name={props.name} color="darkblue">Annat</CategoryButton> : null}
-        </div>
-    )
+            : null} 
 */
