bb8536f0d52d4cb2c3f683cd6aacea15888068fc
Johannes Pettersson <johpe243@student.liu.se>
2023-03-03 16:29:55 +0100

    Fixed a lot of stuffs

--- frontend-friends/src/components/toggle/Toggle.js
+++ frontend-friends/src/components/toggle/Toggle.js
@@ -14,15 +14,36 @@ export default function Toggle() {
         <ReactSwitch 
             checked = {checked}
             onChange = {handleChange}
+            handleDiameter={34}
             className = "toggle"
-            onColor="#FFFFFF" //#F0F1F2
-            offColor="#FFFFFF" //#F0F1F2
-            onHandleColor="#579CFB"
-            offHandleColor="#579CFB"
-            checkedHandleIcon={<MailIcon/>}
-            uncheckedHandleIcon={<PhoneIcon/>}
-            uncheckedIcon={false}
-            checkedIcon={false}
+            onColor="#579CFB" //#F0F1F2
+            offColor="#20E2BA" //#F0F1F2
+            onHandleColor="#FFFFFF"
+            offHandleColor="#FFFFFF"
+            checkedIcon={ <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                  paddingRight: 2
+                }}
+              >
+                <PhoneIcon/>
+              </div>}
+            uncheckedIcon={ <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                  paddingRight: 2
+                }}
+              >
+                <MailIcon/>
+              </div>}
+            width={85}
+            height={40}
         />        
     );
 }
