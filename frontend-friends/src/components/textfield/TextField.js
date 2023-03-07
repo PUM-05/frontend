ea119f5b9ded3e2d6982c06ec60ef7fd571e6951
marta873 <marta873@student.liu.se>
2023-03-07 16:58:10 +0100

    change way of red star

--- frontend-friends/src/components/textfield/TextField.js
+++ frontend-friends/src/components/textfield/TextField.js
@@ -4,11 +4,12 @@ export default function TextField(props) {
     const isRequired = props.isRequired || false;
     if (isRequired) {
         return (
-            <div className="mandatory-text-field">
-                <input type="text" required/>
-                <div className="placeholder">
+            <div className="mandatory-text-field text-field">
+                <input type="text" id="test" name="test" required/>
+                <label for="test">{props.placeholder }</label>
+                {/* <div className="placeholder">
                     {props.placeholder} <span>*</span>
-                </div>
+                </div> */}
             </div>
         );
     }
