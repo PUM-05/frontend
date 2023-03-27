bb3c8873302adc65e1c89ad974213d960188500e
Alfons Johansson <alfons@tacam.se>
2023-03-27 10:23:27 +0200

    Added function for sending post request

---
+++ frontend-friends/src/utils/request.js
@@ -0,0 +1,14 @@
+const baseAPIUrl = 'http://localhost:8000/api'
+
+export async function postData(path, data) {
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+            ''
+        },
+        body: JSON.stringify(data),
+    }
+    return await fetch(baseAPIUrl + path, options)
+}
