d3ba230358f5c9ca8142896ba423fd8985d6c2c8
Filip Johnsson <johnsson.filip@protonmail.com>
2023-03-30 12:03:30 +0200

    Add eslint env in test file

--- frontend-friends/src/App.test.js
+++ frontend-friends/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
+/* eslint-env jest */
+
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/learn react/i)
