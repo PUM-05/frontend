372d4e24e80ad683c263cc5d271af7d72815ff05
unknown <ponfe408@student.liu.se>
2023-04-01 14:58:42 +0200

    Added code to support redirect of unauthorized users. Works with hard-coded values. Need to connect with backend and login-form

--- frontend-friends/src/components/pagewrapper/PageWrapper.js
+++ frontend-friends/src/components/pagewrapper/PageWrapper.js
@@ -1,11 +1,22 @@
-import './pagewrapper.scss'
-import SideBar from '../sidebar/SideBar'
+import './pagewrapper.scss';
+import SideBar from '../sidebar/SideBar';
+import { useEffect , useState} from 'react';
+import { Navigate } from 'react-router-dom';
 
 export default function PageWrapper (props) {
-  return (
-    <div className={'page-container ' + (props.className || '')}>
-      <SideBar />
-      <div className='content-container'>{props.children}</div>
-    </div>
-  )
+  const [authenticated, setauthenticated] = useState(null);
+  const loggedIn = null;// = *check if token exists*
+  if (loggedIn) {
+    setauthenticated(loggedIn);
+  }
+  if (!authenticated){
+    return <Navigate replace to="/login" />;
+  } else{
+    return (
+      <div className={'page-container ' + (props.className || '')}>
+        <SideBar />
+        <div className='content-container'>{props.children}</div>
+      </div>
+    )
+  }
 }
