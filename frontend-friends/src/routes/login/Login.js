1e3c75624013852cbea00a01df765ae51f341212
Marc Taylor <marc@taylorit.com>
2023-04-03 20:59:02 +0200

    add look for admin when logging in

--- frontend-friends/src/routes/login/Login.js
+++ frontend-friends/src/routes/login/Login.js
@@ -3,35 +3,80 @@ import TextField from '../../components/textfield/TextField'
 import './Login.scss'
 import Logo from '../../components/sidebar/logo/Logo'
 import { useState } from 'react'
+import { postData } from '../../utils/request'
+
 
 export default function Input () {
   const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-  return (
+  async function submitData (e) {
+    e.preventDefault()
+    const data = {
+      username: username
+    }
+    await postData('/login', data)
+  }
+  const code=200
+  if (code==200){
+    return(
     <>
-      <div className='contain-all'>
-        <div class='default-logo'>
-          <Logo className='login-logo' />
+        <div className='contain-all'>
+          <div class='default-logo'>
+            <Logo className='login-logo' />
+          </div>
+          <div className='login-form-container'>
+            <form>
+              <TextField
+                placeholder='Användarnamn'
+                isRequired
+                onChange={(e) => {
+                  setUsername(e.target.value)
+                }}
+                value={username}
+              />
+              <TextField 
+                placeholder='Lösenord'
+                isRequired
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+                value={password}
+              />
+            </form>
+            <SubmitButton name='submit'>LOGGA IN</SubmitButton>
+          </div>
         </div>
-        <div className='login-form-container'>
-          <form>
-            <TextField
-              placeholder='Användarnamn'
-              isRequired
-              onChange={(e) => {
-                setUsername(e.target.value)
-              }}
-              value={username}
-            />
-          </form>
-          <SubmitButton name='submit'>LOGGA IN</SubmitButton>
-          {
-                // TODO: Om användare är admin, skickas till
-                // ny sida, där man kan skriva in lösenord, när man har klickat på login-knapp?
-                // Eller ska det finnas ett password-field direkt? Förvirrande för "vanliga" användare?
-            }
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <div className='contain-all'>
+          <div class='default-logo'>
+            <Logo className='login-logo' />
+          </div>
+          <div className='login-form-container'>
+            <form>
+              <TextField
+                placeholder='Användarnamn'
+                isRequired
+                onChange={(e) => {
+                  setUsername(e.target.value)
+                }}
+                value={username}
+              />
+            </form>
+            <SubmitButton name='submit'>LOGGA IN</SubmitButton>
+            {
+                  // TODO: Om användare är admin, skickas till
+                  // ny sida, där man kan skriva in lösenord, när man har klickat på login-knapp?
+                  // Eller ska det finnas ett password-field direkt? Förvirrande för "vanliga" användare?
+              }
+          </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
