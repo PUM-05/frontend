90c0b94b9ffc280310666a2009af2badf7d823ef
Alfons Johansson <alfons@tacam.se>
2023-04-03 12:02:44 +0200

    Fixed React errors

--- frontend-friends/src/components/sidebar/icons/DashboardIcon.js
+++ frontend-friends/src/components/sidebar/icons/DashboardIcon.js
@@ -1,41 +1,11 @@
 export default function DashboardIcon () {
   return (
     <>
-      <svg
-        width='24'
-        height='24'
-        viewBox='0 0 24 24'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <circle
-          cx='5.5'
-          cy='5.5'
-          r='4.25'
-          stroke='currentColor'
-          stroke-width='2.5'
-        />
-        <circle
-          cx='18.5'
-          cy='5.5'
-          r='4.25'
-          stroke='currentColor'
-          stroke-width='2.5'
-        />
-        <circle
-          cx='18.5'
-          cy='18.5'
-          r='4.25'
-          stroke='currentColor'
-          stroke-width='2.5'
-        />
-        <circle
-          cx='5.5'
-          cy='18.5'
-          r='4.25'
-          stroke='currentColor'
-          stroke-width='2.5'
-        />
+      <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <circle cx='5.5' cy='5.5' r='4.25' stroke='currentColor' strokeWidth='2.5' />
+        <circle cx='18.5' cy='5.5' r='4.25' stroke='currentColor' strokeWidth='2.5' />
+        <circle cx='18.5' cy='18.5' r='4.25' stroke='currentColor' strokeWidth='2.5' />
+        <circle cx='5.5' cy='18.5' r='4.25' stroke='currentColor' strokeWidth='2.5' />
       </svg>
     </>
   )
