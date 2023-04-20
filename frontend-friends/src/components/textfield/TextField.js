2aefa1c5cacd92adc014907e2414dc39359d077b
Alfons Johansson <alfons@tacam.se>
2023-04-20 15:49:39 +0200

    Number inputs only has positiv values

--- frontend-friends/src/components/textfield/TextField.js
+++ frontend-friends/src/components/textfield/TextField.js
@@ -14,17 +14,31 @@ export default function TextField (props) {
     inputType = props.type
   }
 
+  /**
+   * Checks if input is positiv integer, if not it prevents the input
+   * @param {*} event Input event
+   */
+  function validatePositivInteger (event) {
+    if (inputType === 'number') {
+      if (!/[0-9]/.test(event.key) && event.key !== 'Backspace' && event.key !== 'Tab') {
+        event.preventDefault()
+      }
+    }
+  }
+
   if (isRequired) {
     return (
       <div className='text-field'>
-        <input className={props.class} type={inputType} id={props.id} name='mandatory-text-field' required onChange={props.onChange} onKeyDown={props.onKeyDown} defaultValue={props.value} />
+        <input
+          onKeyDown={validatePositivInteger} min='0' className={props.class} type={inputType} id={props.id} name='mandatory-text-field' required onChange={props.onChange} defaultValue={props.value}
+        />
         <label className={props.value && 'filled'} htmlFor={props.id}>{props.placeholder}</label>
       </div>
     )
   }
   return (
     <div className='text-field'>
-      <input className={props.class} type={inputType} id={props.id} name='text-field' onChange={props.onChange} onKeyDown={props.onKeyDown} defaultValue={props.value} />
+      <input onKeyDown={validatePositivInteger} min='0' className={props.class} type={inputType} id={props.id} name='text-field' onChange={props.onChange} defaultValue={props.value} />
       <label className={props.value && 'filled'} htmlFor={props.id}>{props.placeholder}</label>
     </div>
   )
