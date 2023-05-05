cd375fe6cd6427742d737c56ce9413e247e292d0
Pontus Ferm <pontus.ferm1@gmail.com>
2023-05-05 15:27:04 +0200

    Added basic layout to statistics page

--- frontend-friends/src/routes/statistics/Statistics.js
+++ frontend-friends/src/routes/statistics/Statistics.js
@@ -15,92 +15,77 @@ export default function Statistics () {
 
   const [interval, setInterval] = useState(7)
   const handleChange = (event) => {
-    switch(event.target.value){
+    switch (event.target.value) {
       case '2Week':
-          setInterval(14)
-          break
+        setInterval(14)
+        break
       case '4Week':
-          setInterval(4)
-          break
+        setInterval(4)
+        break
       case 'year':
-          setInterval(12)
-          break
+        setInterval(12)
+        break
       default:
-          setInterval(7)
-          break
+        setInterval(7)
+        break
     }
-  };
+  }
 
   useEffect(() => {
+    /**
+     * Sends get requests that obtains the number of cases for a certain interval
+     * @param {*} interval is an integer that represents what interval should be requested
+     */
     async function getNumOfCasesWeek (interval) {
       let i = 0
-      let urlArray = []
+      const urlArray = []
       let year = today.getFullYear()
-      let month = today.getMonth()+1
+      let month = today.getMonth() + 1
       let day = today.getDay()
 
-      while(i < interval){
-        if(day === 0 || (day === 7 && interval === 4)){ // new month
+      while (i < interval) {
+        if (day === 0 || (day === 7 && interval === 4)) { // new month
           month = month - 1
-          let newDate = new Date(year, month, 0);
+          const newDate = new Date(year, month, 0)
           day = newDate.getDate()
-        } else if (month === 1 && day <= 0){ // new year
+        } else if (month === 1 && day <= 0) { // new year
           month = 12
           year = year - 1
           day = 31
         }
-        if (interval === 7 || interval === 14){
-          urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + day).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + day).slice(-2)) + 'T23:59:59Z'))
+        if (interval === 7 || interval === 14) {
+          urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ('0' + day).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ('0' + day).slice(-2)) + 'T23:59:59Z'))
           day = day - 1
-        } else if (interval === 4){
-          urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + (day-7)).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + day).slice(-2)) + 'T23:59:59Z'))
+        } else if (interval === 4) {
+          if (day - 7 <= 0) {
+            const monthUpdated = month - 1
+            const diff = Math.abs(day - 7)
+            const newDate = new Date(year, month, 0)
+            const dayUpdated = newDate.getDate() - diff
+            urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (monthUpdated)).slice(-2) + '-' + ('0' + dayUpdated).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ('0' + day).slice(-2)) + 'T23:59:59Z'))
+            day = dayUpdated
+            month = monthUpdated
+          } else {
+            urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ('0' + (day - 7)).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ('0' + day).slice(-2)) + 'T23:59:59Z'))
+          }
           day = day - 7
+        } else {
+          if (month <= 0) {
+            year = year - 1
+            month = 12
+          }
+          const lastDayOfMonth = new Date(today.getFullYear(), month, 0).getDate()
+          urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + '01' + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + lastDayOfMonth) + 'T23:59:59Z')))
+          month = month - 1
         }
         i = i + 1
       }
-      /*
-      if (interval === 7 || interval === 14){
-        while(i < interval){
-          if(day === 1){ // new month
-            month = month - 1
-            let newDate = new Date(year, month, 0);
-            day = newDate.getDate()
-          } else if (month === 1 && day <= 0){ // new year
-            month = 12
-            year = year - 1
-            day = 31
-          }
-          day = day - 1
-          urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + day).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + day).slice(-2)) + 'T23:59:59Z'))
-          i = i + 1
-        }
-      }else if (interval === 4){
-        while(i < interval){
-          if(day === 1){ // new month
-            month = month - 1
-            let newDate = new Date(year, month, 0);
-            day = newDate.getDate()
-          } else if (month === 1 && day <= 0){ // new year
-            month = 12
-            year = year - 1
-            day = 31
-          }
-          urlArray.push(('/stats/medium?start-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + (today.getDate()-((i+1)*7))).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + ('0' + (month)).slice(-2) + '-' + ("0" + (today.getDate()-(i*7))).slice(-2)) + 'T23:59:59Z'))
-          i = i + 1
-        }
-      }/*else{
-        while(i < interval){
-          let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
-          urlArray.push(('/stats/medium?start-time=' + (year + '-' + month + '-' + ("0" + (today.getDate()-((i+1)*7))).slice(-2)) + 'T00:00:00Z&end-time=' + (year + '-' + month + '-' + ("0" + (today.getDate()-(i*7))).slice(-2)) + 'T23:59:59Z'))
-        }
-      }*/
 
-      let promises = urlArray.map(async url => (await getData(url)).json())
-      let phoneArray = []
-      let mailArray = []
-      let array = await Promise.all(promises)
-      console.log(urlArray)
-      for(i=0; i < array.length; i++){
+      const promises = urlArray.map(async url => (await getData(url)).json())
+      const phoneArray = []
+      const mailArray = []
+      const array = await Promise.all(promises)
+      for (i = 0; i < array.length; i++) {
         phoneArray.push(array[i].phone)
         mailArray.push(array[i].email)
       }
@@ -110,47 +95,60 @@ export default function Statistics () {
     getNumOfCasesWeek(interval)
   }, [interval])
 
-  function setLabels(interval){
+  /**
+   * Retrieves the correct labels for the x-axis of the given interval
+   * @param {*} interval is the interval that should be displayed in the chart
+   * @returns an array of labels
+   */
+  function setLabels (interval) {
     let year = today.getFullYear()
-    let month = today.getMonth()+1
+    let month = today.getMonth() + 1
     let day = today.getDay()
     let i = 0
-    let labelArray = []
-    const monthNames = ['','January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
+    const labelArray = []
+    const monthNames = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-    while(i < interval){
-      if(day === 0 || (day === 7 && interval === 4)){ // new month
+    while (i < interval) {
+      // Check for new month or new year
+      if (day === 0 || (day === 7 && interval === 4)) { // new month
         month = month - 1
-        let newDate = new Date(year, month, 0);
+        const newDate = new Date(year, month, 0)
         day = newDate.getDate()
-      } else if (month === 1 && day <= 0){ // new year
+      } else if (month === 1 && day <= 0) { // new year
         month = 12
         year = year - 1
         day = 31
       }
-      if(interval === 7 || interval === 14){  
-        labelArray.push((year + '-' + month + '-' + (("0" + day).slice(-2))))
+
+      // Check the interval. 7 or 14 means data from each day for 7 or 14 days
+      if (interval === 7 || interval === 14) {
+        labelArray.push((year + '-' + month + '-' + (('0' + day).slice(-2))))
         day = day - 1
-      }else if (interval === 4){
-        if(day - 7 <= 0){
-          let monthUpdated = month - 1
-          let diff = Math.abs(day-7)
-          let newDate = new Date(year, month, 0);
-          let dayUpdated = newDate.getDate() - diff
-          labelArray.push(((dayUpdated) + ' ' + monthNames[monthUpdated] +'-' + day + ' ' + monthNames[month].slice(0,3)))
+      } else if (interval === 4) { // 4 means 4 weeks worth of data (roughly one month)
+        if (day - 7 <= 0) {
+          const monthUpdated = month - 1
+          const diff = Math.abs(day - 7)
+          const newDate = new Date(year, month, 0)
+          const dayUpdated = newDate.getDate() - diff
+          labelArray.push(((dayUpdated) + ' ' + monthNames[monthUpdated].slice(0, 3) + '-' + day + ' ' + monthNames[month].slice(0, 3)))
           day = dayUpdated
           month = monthUpdated
-        } else{
-          labelArray.push(((day-7) + '-' + day + monthNames[month].slice(0,3)))
+        } else {
+          labelArray.push(((day - 7) + '-' + day + ' ' + monthNames[month].slice(0, 3)))
         }
         day = day - 7
-      } 
-      i = i + 1  
+      } else { // returns labels for 12 months, starting from the current month
+        if (month <= 0) {
+          year = year - 1
+          month = 12
+        }
+        labelArray.push(monthNames[month].slice(0, 3))
+        month = month - 1
+      }
+      i = i + 1
     }
-    console.log(labelArray)
     return labelArray.slice().reverse()
   }
-
 
   return (
     <>
@@ -158,24 +156,25 @@ export default function Statistics () {
         <h1>Statistics</h1>
         <div className='chart-container'>
           <div className='barchart-container'>
-            <IntervalDropdown onChange={handleChange}/>
-            <BarChart 
+            <IntervalDropdown onChange={handleChange} />
+            <BarChart
               labels={setLabels(interval)}
               titletext='Antal ärenden'
-              datasets= {[
+              datasets={[
                 {
                   label: 'Telefon',
                   data: numOfCalls,
-                  backgroundColor: '#579CFB',
+                  backgroundColor: '#579CFB'
                 },
                 {
                   label: 'Mail',
                   data: numOfMails,
-                  backgroundColor: '#20E2BA',
+                  backgroundColor: '#20E2BA'
                 }
               ]}
             />
           </div>
+          {/* Add other charts here */}
         </div>
       </PageWrapper>
     </>
