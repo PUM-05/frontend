2bf785f33ecd0bf064d5be3b006d80da166352f0
Pontus Ferm <pontus.ferm1@gmail.com>
2023-05-05 15:26:18 +0200

    Added barChart + fixed style

--- frontend-friends/src/components/barChart/barChart.js
+++ frontend-friends/src/components/barChart/barChart.js
@@ -1,25 +1,24 @@
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
 import { Bar } from 'react-chartjs-2'
 
 import './barChart.scss'
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 /**
  * Bar chart displaying the given data
@@ -27,19 +26,16 @@ ChartJS.register(
  * @returns a bar chart component
  */
 export default function barChart (props) {
-  let xVals = ["Italy", "France", "Spain", "USA", "Argentina"]
-  let yVals = [55, 49, 44, 24, 15]
-
   const data = {
     labels: props.labels,
-    datasets: props.datasets,
+    datasets: props.datasets
   }
 
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        position: 'top'
       },
       title: {
         display: true,
@@ -53,38 +49,16 @@ export default function barChart (props) {
           size: 15,
           family: ['Montserrat', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif']
         }
-      },
+      }
     },
     scales: {
       x: {
-        stacked: true,
+        stacked: true
       },
       y: {
-        stacked: true,
-      },
-    },
-  }
-
-  const oldoptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    maxHeight: 50,
-    plugins: {
-      title: {
-        display: true,
-        position: 'top',
-        text: props.titletext,
-        padding: {
-          top: 10,
-          bottom: 10
-        },
-        font: {
-          size: 24,
-          family: ['Montserrat', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif']
-        }
-      },
+        stacked: true
+      }
     }
-
   }
 
   return (
