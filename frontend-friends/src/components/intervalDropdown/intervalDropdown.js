b51f76f1f8947c0a64c6b17d88dea5c5a3529d57
Pontus Ferm <123727457+PontusFerm@users.noreply.github.com>
2023-05-17 12:38:49 +0200

    Time per category chart (#71)
    * Things didn't go as planned with my other branch so I created a new one with the changes here instead
    
    * Added some comments and fixed style
    
    * Things didn't go as planned with my other branch so I created a new one with the changes here instead
    
    * Added some comments and fixed style
    
    * Changed from all tid to totalt

--- frontend-friends/src/components/intervalDropdown/intervalDropdown.js
+++ frontend-friends/src/components/intervalDropdown/intervalDropdown.js
@@ -6,13 +6,19 @@ import './intervalDropdown.scss'
  * @returns dropdown component
  */
 export default function IntervalDropdown (props) {
+  function getOptions () {
+    const options = []
+    for (let i = 0; i < Object.keys(props.options).length; i++) {
+      options.push(<option key={i} value={Object.keys(props.options)[i]}>{Object.values(props.options)[i]}</option>)
+    }
+    return options
+  }
+
   return (
     <div className='dropdown-container'>
       <select className='time-select' onChange={props.onChange} value={props.value}>
-        <option value='week'>1 vecka</option>
-        <option value='2Week'>2 veckor</option>
-        <option value='4Week'>4 veckor</option>
-        <option value='year'>1 år</option>
+        {getOptions()}
+
       </select>
     </div>
   )
