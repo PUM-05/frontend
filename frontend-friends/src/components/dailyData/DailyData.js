0f2013a8e397b8ad77b836ed8279099535b43590
Marc Taylor <marc@taylorit.com>
2023-05-22 09:59:20 +0200

    Fixed basic layout for daily info

--- frontend-friends/src/components/dailyData/DailyData.js
+++ frontend-friends/src/components/dailyData/DailyData.js
@@ -1,5 +1,5 @@
 import './daily-data.scss'
-import MailIcon from './icons/MailIcon';
+import MailIcon from './icons/mailIcon.svg';
 import PhoneIcon from './icons/phoneIcon.svg';
 
 export default function DailyData (props) {
@@ -9,28 +9,28 @@ export default function DailyData (props) {
   return (
     <div className='daily-data-container'>
       <div className='daily-total daily-data'>
-        <p className='data-title'>TOTALT</p>
         <p className='amount'>{totalAmount} st</p>
         <p className='errands-text'>Ärenden idag</p> 
         <p className='data-comparison'>
           <span className='percentage'>ärenden från igår</span>
         </p>
+        <p className='data-title'>TOTALT</p>
       </div>
       <div className='phone-data daily-data'>
-        <div className='data-pic'>
-          <img src={PhoneIcon}></img>
-        </div>
         <p className='amount'>{phoneAmount} st</p>
         <p className='errands-text'>Ärenden</p>
         <p className='data-comparison'> ärenden från igår</p>
+        <div className='data-pic'>
+          <img src={PhoneIcon} />
+        </div>
       </div>
       <div className='mail-data daily-data'>
-        <div className='data-pic'>
-          <MailIcon />
-        </div>
         <p className='amount'>{mailAmount} st</p>
         <p className='errands-text'>Ärenden</p>
         <p className='data-comparison'> ärenden från igår</p>
+        <div className='data-pic'>
+          <img src={MailIcon} />
+        </div>
       </div>
     </div>
   )
