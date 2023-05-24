2a539da0cca6ba0276e9cc10dc4c8cb29572d25f
Klumpen <klumpen551@gmail.com>
2023-05-24 11:06:44 +0200

    format

--- frontend-friends/src/components/customPieCharts/mediumPieChart.js
+++ frontend-friends/src/components/customPieCharts/mediumPieChart.js
@@ -8,11 +8,11 @@ import { getData } from '../../utils/request'
  * @param {*} props
  * @returns Component containing two pie charts
  */
-export default function PieChartGroup (props) {
+export default function PieChartGroup(props) {
   const [mediumData, setMediumData] = useState([])
   const [interval, setInterval] = useState(100)
   useEffect(() => {
-    async function getPieData () {
+    async function getPieData() {
       const today = new Date()
       let startYear = today.getFullYear()
       let startMonth = today.getMonth() + 1
@@ -20,19 +20,19 @@ export default function PieChartGroup (props) {
       const endYear = today.getFullYear()
       const endMonth = today.getMonth() + 1
       const endDay = today.getDate()
-      let i = interval-1;
-      while(i > 0){
-        if(startDay === 1){
-            if(startMonth === 1){
-                startYear -= 1
-                startMonth = 12
-            }else{
-                startMonth -= 1
-            }
-            const newDate = new Date(startYear, startMonth, 0)
-            startDay = newDate.getDate()
-        }else{
-            startDay -= 1
+      let i = interval - 1;
+      while (i > 0) {
+        if (startDay === 1) {
+          if (startMonth === 1) {
+            startYear -= 1
+            startMonth = 12
+          } else {
+            startMonth -= 1
+          }
+          const newDate = new Date(startYear, startMonth, 0)
+          startDay = newDate.getDate()
+        } else {
+          startDay -= 1
         }
         i -= 1
       }
@@ -80,7 +80,7 @@ export default function PieChartGroup (props) {
   }
   return (
     <div>
-        <IntervalDropdown onChange={handleChange} options={{ alltime: 'Totalt', week: '1 vecka', '2Week': '2 veckor', '4Week': '4 veckor', year: '1 år' }} />
+      <IntervalDropdown onChange={handleChange} options={{ alltime: 'Totalt', week: '1 vecka', '2Week': '2 veckor', '4Week': '4 veckor', year: '1 år' }} />
       <PieChart
         labels={['Telefon', 'Mail']}
         titletext='Medie'
